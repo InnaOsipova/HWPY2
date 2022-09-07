30ec2df09a6dd098cb5088a39fb1e8df7bc16f16
Inna Osipova <rutheva_inna@mail.ru>
2022-09-07 13:41:17 +0300

    Исправление перемешивания списка

--- dz6.py
+++ dz6.py
@@ -1,22 +1,18 @@
 #Реализуйте алгоритм перемешивания списка.
 
-
 import string
-
 
 def mixing(l, num, lk):
     import random
-    import copy
-    l1 = copy.deepcopy(l)
     if num == 1:
-        lk.append(l1[num])
-        lk.append(l1[num-1])
+        lk.append(l[num])
+        lk.append(l[num-1])
         return 
     else:
         index = random.randint(0,num-1)
-        l1[index], l1[num] = l1[num], l1[index]
-        lk.append(l1[num])
-        mixing (l1, num-1, lk)
+        l[index], l[num] = l[num], l[index]
+        lk.append(l[num])
+        mixing (l, num-1, lk)
     return lk
 
 initial_list = [1, 2, 3, 4, 5, 6, 7, 8]
